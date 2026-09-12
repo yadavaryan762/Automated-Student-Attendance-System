1006f7ee73cad38d2c905ef7b6afb048bcd5103a
Aryan <yadavaryan3110@gmail.com>
2026-09-12 20:28:24 +0530

    Add student list functionality

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const readline = require("readline");
+const fs = require("fs");
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+const studentsFile = "./students.json";
 
 function showMenu() {
     console.log("\n========================================");
@@ -17,28 +20,53 @@ function showMenu() {
     console.log("========================================");
 }
 
+function viewStudents() {
+
+    const data = fs.readFileSync(studentsFile, "utf-8");
+    const studentsData = JSON.parse(data);
+
+    console.log("\n========================================");
+    console.log("             STUDENT LIST");
+    console.log("========================================");
+
+    studentsData.students.forEach((student) => {
+        console.log(
+            `${student.id}. ${student.name} - Class ${student.class}`
+        );
+    });
+
+    console.log("========================================");
+
+    startApp();
+}
+
 function startApp() {
+
     showMenu();
 
     rl.question("Enter your choice: ", (choice) => {
 
         if (choice === "1") {
-            console.log("\nStudent list module will be added soon.");
-            startApp();
+
+            viewStudents();
 
         } else if (choice === "2") {
+
             console.log("\nAttendance module will be added soon.");
             startApp();
 
         } else if (choice === "3") {
+
             console.log("\nReport module will be added soon.");
             startApp();
 
         } else if (choice === "4") {
+
             console.log("\nThank you for using the system.");
             rl.close();
 
         } else {
+
             console.log("\nInvalid choice. Please enter 1-4.");
             startApp();
         }
